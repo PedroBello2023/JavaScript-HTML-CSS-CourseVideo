1a49174ee74ce0b32d83095aefdec4e5c0452b59
Pedro Henrique Martins Belo <pedrohenriqueufpa2018@gmail.com>
2023-10-13 22:31:44 -0300

    JavaScript.

---
+++ Aula 06/ex02.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é o seu nome? ')
+document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras.</br>`)//Escreve na tela a mensagem.
+document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()}.</br>`)
+document.write(`Seu nome em maiúsculo é ${nome.toLowerCase()}.`)
---
+++ Aula 06/ex03.js
@@ -0,0 +1,5 @@
+var n1 = Number.parseFloat(window.prompt('Digite um número: '))
+var n2 = Number.parseFloat(window.prompt('Digite outro número: '))
+s = n1 + n2
+
+window.alert(`A soma de ${n1} com ${n2} é ${s}.`)
